7e5b9f53b5c8c0e23644bf554712ba5e8c58ed89
MSure-GP03D <ohsono@msure.co.jp>
2025-07-15 15:36:11 +0900

    初めてのコミット

--- action_item_bot/settings.py
+++ action_item_bot/settings.py
@@ -53,14 +53,15 @@ ROOT_URLCONF = "action_item_bot.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # この行を追加
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 # action_item_bot/urls.py (修正)
-from django.contrib import admin
-from django.urls import path, include
+from django.http import HttpResponse
+from django.urls import path
+
+def hello(request):
+    return HttpResponse("Hello World")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', hello, name='hello')
 ]
